804f0a94d4f7403a1a0142b6d56fecbd54977394
unknown <shangeeran@gmail.com>
2020-05-18 23:12:53 +0000

    Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
+      <EventBind />
       {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman">
@@ -18,9 +20,9 @@ function App() {
       <Greet name="Tino" heroName="Superman" />
       <button>Action</button>
       <Greet name="Thanu" heroName="Wonder Woman" /> */}
-      <Welcome name="Bruce" heroName="Batman"/>
+      {/* <Welcome name="Bruce" heroName="Batman"/>
       <Welcome name="Tino" heroName="Superman"/>
-      <Welcome name="Thanu" heroName="Wonder Woman"/>
+      <Welcome name="Thanu" heroName="Wonder Woman"/> */}
       {/* <Hello /> */}
     </div>
   );
